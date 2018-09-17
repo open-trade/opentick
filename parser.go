e8c03bf128f13828a6f4efecbe26a7c6602344e9
open-trade <info@opentradesolutions.com>
2018-09-17 09:55:04 +0800

    remove Reduce/Flattern, remove IN/OR/()/!=/<> to make sql expression simplest

--- parser.go
+++ parser.go
@@ -7,11 +7,11 @@ import (
 
 var (
 	sqlLexer = lexer.Must(lexer.Regexp(`(\s+)` +
-		`|(?P<Keyword>(?i)\b(TIMESTAMP|DATABASE|BOOLEAN|PRIMARY|SMALLINT|TINYINT|BIGINT|DOUBLE|SELECT|INSERT|VALUES|CREATE|DELETE|RENAME|FLOAT|WHERE|LIMIT|TABLE|ALTER|FALSE|TEXT|FROM|TYPE|DROP|TRUE|INTO|ADD|AND|KEY|INT|OR|IN)\b)` +
+		`|(?P<Keyword>(?i)\b(TIMESTAMP|DATABASE|BOOLEAN|PRIMARY|SMALLINT|TINYINT|BIGINT|DOUBLE|SELECT|INSERT|VALUES|CREATE|DELETE|RENAME|FLOAT|WHERE|LIMIT|TABLE|ALTER|FALSE|TEXT|FROM|TYPE|DROP|TRUE|INTO|ADD|AND|KEY|INT)\b)` +
 		`|(?P<Ident>[a-zA-Z][a-zA-Z0-9_]*)` +
 		`|(?P<Number>-?\d*\.?\d+([eE][-+]?\d+)?)` +
 		`|(?P<String>'[^']*'|"[^"]*")` +
-		`|(?P<Operator><>|!=|<=|>=|[-+*/%,.()=<>?])`,
+		`|(?P<Operator><=|>=|[-+*/%,.()=<>?])`,
 	))
 	sqlParser = participle.MustBuild(
 		&Ast{},
@@ -94,28 +94,12 @@ type AstSelectExpression struct {
 }
 
 type AstExpression struct {
-	OrAnd []AstAndCondition `@@ {"OR" @@}`
-	And   []AstCondition
-	Or    []AstCondition
-}
-
-type AstAndCondition struct {
 	And []AstCondition `@@ {"AND" @@}`
 }
 
 type AstCondition struct {
-	LHS           *string          `@Ident`
-	ConditionRHS  *AstConditionRHS `[@@]`
-	SubExpression *AstExpression   `| "(" @@ ")"`
-}
-
-type AstConditionRHS struct {
-	Compare *AstCompare `@@`
-	In      []AstValue  `| "IN" "(" @@ {"," @@} ")"`
-}
-
-type AstCompare struct {
-	Operator *string   `@("<>" | "<=" | ">=" | "=" | "<" | ">" | "!=")`
+	LHS      *string   `@Ident`
+	Operator *string   `@("<=" | ">=" | "=" | "<" | ">")`
 	RHS      *AstValue `@@`
 }
 
@@ -124,63 +108,6 @@ type AstValue struct {
 	String      *string     `| @String`
 	Placeholder *string     `| "?"`
 	Boolean     *AstBoolean `| @("TRUE" | "FALSE")`
-}
-
-// reduce OrAnd
-func (expr *AstExpression) Reduce() {
-	for i := 0; i < len(expr.OrAnd); i++ {
-		for j := 0; j < len(expr.OrAnd[i].And); j++ {
-			c := &expr.OrAnd[i].And[j]
-			if c.SubExpression != nil {
-				c.SubExpression.Reduce()
-			}
-		}
-	}
-	if len(expr.OrAnd) == 1 {
-		expr.And = expr.OrAnd[0].And
-	} else {
-		conds := make([]AstCondition, len(expr.OrAnd))
-		for i := 0; i < len(expr.OrAnd); i++ {
-			if len(expr.OrAnd[i].And) == 1 {
-				conds[i] = expr.OrAnd[i].And[0]
-			} else {
-				tmp := &AstExpression{}
-				tmp.And = expr.OrAnd[i].And
-				conds[i].SubExpression = tmp
-			}
-		}
-		expr.Or = conds
-	}
-	expr.OrAnd = nil
-}
-
-func (expr *AstExpression) Flatten() {
-	if expr.OrAnd != nil {
-		panic("Please Reduce first")
-	}
-
-	if expr.And != nil {
-		for i := 0; i < len(expr.And); {
-			if expr.And[i].SubExpression != nil {
-				expr.And[i].SubExpression.Flatten()
-				if expr.And[i].SubExpression.And != nil {
-					and2 := expr.And[i].SubExpression.And
-					expr.And[i] = and2[0]
-					expr.And = append(expr.And, and2[1:]...)
-					i += len(and2)
-					continue
-				} else {
-				}
-			}
-			i += 1
-		}
-	} else {
-		for i := 0; i < len(expr.Or); i++ {
-			if expr.Or[i].SubExpression != nil {
-				expr.Or[i].SubExpression.Flatten()
-			}
-		}
-	}
 }
 
 func Parse(sql string) (*Ast, error) {
--- parser_test.go
+++ parser_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-var sqlSelectStmt = "select a, b from test where (a > 1.2) and (b < 2 or (b in (1,2))) limit -2"
+var sqlSelectStmt = "select a, b from test where a > 1.2 and b < 2 limit -2"
 var sqlInsertStmt = "INSERT into x(x, y) values(1, 2)"
 var sqlInsertAst = `
 &opentick.Ast{
@@ -46,43 +46,16 @@ var sqlSelectAst = `
       And: []opentick.AstCondition{
         opentick.AstCondition{
           LHS: &"a",
-          ConditionRHS: &opentick.AstConditionRHS{
-            Compare: &opentick.AstCompare{
-              Operator: &">",
-              RHS: &opentick.AstValue{
-                Number: &1.2,
-              },
-            },
+          Operator: &">",
+          RHS: &opentick.AstValue{
+            Number: &1.2,
           },
         },
         opentick.AstCondition{
-          SubExpression: &opentick.AstExpression{
-            Or: []opentick.AstCondition{
-              opentick.AstCondition{
-                LHS: &"b",
-                ConditionRHS: &opentick.AstConditionRHS{
-                  Compare: &opentick.AstCompare{
-                    Operator: &"<",
-                    RHS: &opentick.AstValue{
-                      Number: &2,
-                    },
-                  },
-                },
-              },
-              opentick.AstCondition{
-                LHS: &"b",
-                ConditionRHS: &opentick.AstConditionRHS{
-                  In: []opentick.AstValue{
-                    opentick.AstValue{
-                      Number: &1,
-                    },
-                    opentick.AstValue{
-                      Number: &2,
-                    },
-                  },
-                },
-              },
-            },
+          LHS: &"b",
+          Operator: &"<",
+          RHS: &opentick.AstValue{
+            Number: &2,
           },
         },
       },
@@ -94,13 +67,7 @@ var sqlSelectAst = `
 
 func Test_Parse(t *testing.T) {
 	expr, err := Parse(sqlSelectStmt)
-	expr.Select.Where.Reduce()
-	// assert.Equal(t, repr.String(expr, repr.Indent("  "), repr.OmitEmpty(true)), strings.TrimSpace(sqlSelectAst))
-	repr.Println(expr, repr.Indent("  "), repr.OmitEmpty(true))
-	expr, err = Parse("select * from a where (((a=1 and (b=2.2 and ((c=3 and (d=3))))))) limit -2")
-	expr.Select.Where.Reduce()
-	expr.Select.Where.Flatten()
-	repr.Println(expr, repr.Indent("  "), repr.OmitEmpty(true))
+	assert.Equal(t, repr.String(expr, repr.Indent("  "), repr.OmitEmpty(true)), strings.TrimSpace(sqlSelectAst))
 	assert.Equal(t, err, nil)
 	expr, err = Parse(sqlInsertStmt)
 	assert.Equal(t, repr.String(expr, repr.Indent("  "), repr.OmitEmpty(true)), strings.TrimSpace(sqlInsertAst))
